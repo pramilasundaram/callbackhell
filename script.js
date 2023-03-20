19de65293b8d619e73a83b2eb4b225a9e2b887ba
unknown <pramilaworld@gmail.com>
2023-03-20 09:47:10 -0700

    callback hell

---
+++ script.js
@@ -0,0 +1,47 @@
+let input=document.getElementById("hi");
+ let num=10;
+setTimeout(( )=>{
+    input.innerText=num;
+    num--;
+    setTimeout(()=>{
+        input.innerText=num;
+        num--;
+        setTimeout(()=>{
+            input.innerText=num;
+            num--;
+            setTimeout(()=>{
+                input.innerText=num;
+                num--;
+                setTimeout(()=>{
+                    input.innerText=num;
+                    num--;
+                    setTimeout(()=>{
+                        input.innerText=num;
+                        num--;
+                        setTimeout(()=>{
+                            input.innerText=num;
+                            num--;
+                            setTimeout(()=>{
+                                input.innerText=num;
+                                num--;
+                                setTimeout(()=>{
+                                    input.innerText=num;
+                                    num--;
+                                    setTimeout(()=>{
+                                        input.innerText=num;
+                                        num--;
+                                        setTimeout(()=>{
+                                            input.innerText="happy independence day";
+                                            
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+
